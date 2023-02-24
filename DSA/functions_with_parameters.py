d22439281ce806a87a4a6a6d73f5eeee9aaa1a86
Sudi Leon <sudileon@LILIANs-MacBook-Air.local>
2023-02-24 15:53:54 +0300

    modified function

---
+++ DSA/functions_with_parameters.py
@@ -0,0 +1,9 @@
+def addtwonums(num1, num2) :
+    results = num1+num2
+    print(results)
+addtwonums(200,50)
+
+def multiplynums(num3,num4):
+    results = num3 * num4
+    print(results)
+multiplynums(2500,2)
---
+++ function_exercise.py
@@ -0,0 +1,6 @@
+def calculate_months_live():
+    age = int(input("enter your age"))
+    months_lived = age * 12
+    print(f"you have lived {months_lived} months")
+
+calculate_months_live()
